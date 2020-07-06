6b7f31e6f4e3404024e83c784ae8d7c4c4a8a6f8
k1ns3 <k1ns3za@gmail.com>
2020-07-06 17:23:06 +0300

    minor fixes

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 function Page404() {
   return (
     <div className="wrapper">
-      <h1>Такой страницы не существует 😵</h1>
+      <h1>
+        Такой страницы не существует <span role="img" aria-label="emoji">😵</span>
+      </h1>
       <Link to="/">Перейти на главнгую</Link>
     </div>
   );
